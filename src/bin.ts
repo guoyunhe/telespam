600310b3210f1b9ee011c4628113d8121ec9f30a
Guo Yunhe <i@guoyunhe.me>
2026-08-24 17:09:20 +0800

    refactor: use commander

--- src/bin.ts
+++ src/bin.ts
@@ -1,53 +1,71 @@
 #!/usr/bin/env node
+import { createRequire } from 'node:module';
+
+import { Command } from 'commander';
+
 import { loadConfig } from './config.js';
 import { Telespam } from './index.js';
 import { cmdInstall, cmdStatus, cmdUninstall } from './systemd.js';
 
-// ---- main ----
+const require = createRequire(import.meta.url);
+const pkg = require('../package.json') as { version: string };
 
-const command = process.argv[2];
+const program = new Command();
 
-switch (command) {
-  case 'install':
+program
+  .name('telespam')
+  .description('self-host anti-spam solution for Telegram')
+  .version(pkg.version);
+
+program
+  .command('install')
+  .description('install as systemd user service')
+  .action(() => {
     cmdInstall();
     process.exit(0);
-  case 'uninstall':
+  });
+
+program
+  .command('uninstall')
+  .description('uninstall systemd user service')
+  .action(() => {
     cmdUninstall();
     process.exit(0);
-  case 'status':
+  });
+
+program
+  .command('status')
+  .description('show systemd service status')
+  .action(() => {
     cmdStatus();
     process.exit(0);
-  case undefined:
-    // Run the bot
-    break;
-  default:
-    console.error(`Unknown command: ${command}`);
-    console.error('Usage: telespam [install|uninstall|status]');
-    process.exit(1);
-}
+  });
 
-// ---- run bot ----
+// Default: run the bot
+program.action(async () => {
+  const configs = loadConfig();
 
-const configs = loadConfig();
-
-// Validate all configs
-for (let i = 0; i < configs.length; i++) {
-  if (!configs[i].apiKey) {
-    console.error(`Missing apiKey in config[${i}] of telespam.json`);
-    process.exit(1);
+  // Validate all configs
+  for (let i = 0; i < configs.length; i++) {
+    if (!configs[i].apiKey) {
+      console.error(`Missing apiKey in config[${i}] of telespam.json`);
+      process.exit(1);
+    }
   }
-}
 
-const bots = configs.map((config) => new Telespam(config));
+  const bots = configs.map((config) => new Telespam(config));
 
-// Graceful shutdown: stop all bots on SIGINT / SIGTERM
-const shutdown = async () => {
-  await Promise.all(bots.map((bot) => bot.stop()));
-  process.exit(0);
-};
+  // Graceful shutdown: stop all bots on SIGINT / SIGTERM
+  const shutdown = async () => {
+    await Promise.all(bots.map((bot) => bot.stop()));
+    process.exit(0);
+  };
 
-process.on('SIGINT', shutdown);
-process.on('SIGTERM', shutdown);
+  process.on('SIGINT', shutdown);
+  process.on('SIGTERM', shutdown);
 
-// Start all bots
-await Promise.all(bots.map((bot) => bot.start()));
+  // Start all bots
+  await Promise.all(bots.map((bot) => bot.start()));
+});
+
+program.parse();
